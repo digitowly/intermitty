64843e7bf1f07c0b5feae8d809c7322537d3ab28
RopFoo <ropfoo@gmail.com>
2021-03-07 20:33:07 +0100

    added stateful wrapper widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:intermitty/modules/counter/counter_screen.dart';
+import 'package:intermitty/widgets/stateful_wrapper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  void loadTimeData() async {
+    final prefs = await SharedPreferences.getInstance();
+    print(prefs.getString('time'));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CounterScreen());
+    return MaterialApp(
+      home: StatefulWrapper(
+        onInit: () {
+          print('Init');
+          loadTimeData();
+        },
+        child: CounterScreen(),
+      ),
+    );
   }
 }
---
+++ lib/widgets/stateful_wrapper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class StatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+
+  StatefulWrapper({@required this.onInit, @required this.child});
+
+  @override
+  _StatefulWrapperState createState() => _StatefulWrapperState();
+}
+
+class _StatefulWrapperState extends State<StatefulWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    widget.onInit();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
